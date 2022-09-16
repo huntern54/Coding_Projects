5a63bb22272a1fbf308fd552099089e60288ff92
Hunter <hunter.nghiem0@gmail.com>
2022-09-16 11:20:53 -0500

    Finished product (basic)
    This takes in 3 inputs and stores into a string that becomes "MadLibs"

--- Madlibs.py
+++ Madlibs.py
@@ -1,8 +1,18 @@
 # string concatenation 
 # suppose we want to create a string that says "subscribe to ___ "
 
-text = "subscribe to:"
-youtuber = ""
+# text = "subscribe to:"
+# youtuber = " Hunter Nghiem"
 
-print (text + youtuber)
-print (text.format(youtuber))
+# CTR + K + C = Comment out
+# CTR + K + U = Uncomment 
+# print (text + youtuber)
+# print ("subcribe to {}".format(youtuber))
+# print(f"{text} {youtuber}")
+adj = input("Adj: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Per: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}" 
+print(madlib)
