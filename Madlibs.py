0e049530c5534abdf4f14a21a1b8b0ecf63cf3f9
Hunter <hunter.nghiem0@gmail.com>
2022-09-16 11:05:49 -0500

    Starting Point
    This is a test commit to put up what i like about gitkraken

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+# string concatenation 
+# suppose we want to create a string that says "subscribe to ___ "
+
+text = "subscribe to:"
+youtuber = ""
+
+print (text + youtuber)
+print (text.format(youtuber))
